16919f0b1288e60e90b28629f4ae8bc50a8a3082
pvysotski <pavelve83@mail.ru>
2020-09-10 10:00:03 +0300

    change CopyElements

--- src/main/java/by/homework/arrays/App.java
+++ src/main/java/by/homework/arrays/App.java
@@ -1,13 +1,19 @@
 package by.homework.arrays;
 
-import java.lang.reflect.Array;
+
 
 public class App {
     public static void main(String[] args) {
 
 
+        int[] array = {1, -2, 5, 10, 4, 8, 7, -5, 16, 6};
+        int leftBound = -5;
+        int rightBound = 5;
 
-
+        ArrayService.printArray(CopyElements.copyInRange(array, leftBound, rightBound));
 
     }
+
 }
+
+
--- src/main/java/by/homework/arrays/CopyElements.java
+++ src/main/java/by/homework/arrays/CopyElements.java
@@ -3,9 +3,19 @@ package by.homework.arrays;
 public class CopyElements {
 
     public static int[] copyInRange(int[] array, int leftBound, int rightBound) {
-        int[] newArray = new int[rightBound - leftBound + 1];
-        for (int i = 0; i < newArray.length; i++) {
-            newArray[i] = array[i + leftBound - 1];
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] >= leftBound && array[i] <= rightBound) {
+                count++;
+            }
+        }
+        int[] newArray = new int[count];
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] >= leftBound && array[i] <= rightBound) {
+                newArray[index] = array[i];
+                index++;
+            }
         }
         return newArray;
     }
